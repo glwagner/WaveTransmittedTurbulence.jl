cd6be01c851a19d305864d36a8760c5a663ad122
Gregory Wagner <wagner.greg@gmail.com>
2020-04-11 15:04:01 -0400

    Take wave parameters from global scope, not stokes drift object

--- simulations/run_growing_wave_forced.jl
+++ simulations/run_growing_wave_forced.jl
@@ -155,9 +155,7 @@ simulation = Simulation(model, Δt=wizard, stop_time=stop_time, progress_frequen
 
 prefix = @sprintf("growing_wave_forced_Qb%.1e_a%.1f_k%.1e_T%.1f_Nh%d_Nz%d",
                   get_parameter(filepath, "boundary_conditions", "Qᵇ"),
-                  stokes_drift.∂z_uˢ.wave_amplitude,
-                  stokes_drift.∂z_uˢ.wave_number,
-                  stokes_drift.∂z_uˢ.growth_time_scale / hour,
+                  wave_amplitude, wave_number, growth_time_scale / hour,
                   model.grid.Nx, model.grid.Nz)
 
 data_directory = joinpath(@__DIR__, "..", "data", prefix) # save data in /data/prefix
